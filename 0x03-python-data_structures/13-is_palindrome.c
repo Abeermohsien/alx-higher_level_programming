1518e17197f9bd0675f85afce1af743aeede9d91
Abeermohsien <aboorakhalil993@gmail.com>
2023-12-04 04:45:21 -0800

    $

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,4 +1,5 @@
-#include "list.h"
+#include "lists.h"
+
 /**
  * is_palindrome - palind or not
  * @head: head
@@ -6,10 +7,11 @@
  */
 int is_palindrome(listint_t **head)
 {
-if (head == NULL || *head == NULL)
-return (1);
-return (check_palind(head, *head));
+	if (head == NULL || *head == NULL)
+		return (1);
+	return (check_palind(head, *head));
 }
+
 /**
  * check_palind - if is palindrome
  * @head: head
@@ -18,12 +20,12 @@ return (check_palind(head, *head));
  */
 int check_palind(listint_t **head, listint_t *end)
 {
-if (end == NULL)
-return (1);
-if (check_palind(head, end->next) && (*head)->n == end->n)
-{
-*head = (*head)->next;
-return (1);
-}
-return (0);
+	if (end == NULL)
+		return (1);
+	if (check_palind(head, end->next) && (*head)->n == end->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
 }
