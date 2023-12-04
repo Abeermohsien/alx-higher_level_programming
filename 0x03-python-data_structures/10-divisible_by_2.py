0532a37b21c312aec2f99548992d2f390f0848db
Abeermohsien <aboorakhalil993@gmail.com>
2023-12-04 04:01:14 -0800

    $

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list is None:
+        return None
+    dilist = []
+    for j in my_list:
+        if (i % 2) == 0:
+            dilist.append(True)
+        else:
+            dilist.append(False)
+    return dilist
---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,29 @@
+#include "list.h"
+/**
+ * is_palindrome - palind or not
+ * @head: head
+ * Return: 0
+ */
+int is_palindrome(listint_t **head)
+{
+if (head == NULL || *head == NULL)
+return (1);
+return (check_palind(head, *head));
+}
+/**
+ * check_palind - if is palindrome
+ * @head: head
+ * @end: end
+ * Return: int
+ */
+int check_palind(listint_t **head, listint_t *end)
+{
+if (end == NULL)
+return (1);
+if (check_palind(head, end->next) && (*head)->n == end->n)
+{
+*head = (*head)->next;
+return (1);
+}
+return (0);
+}
