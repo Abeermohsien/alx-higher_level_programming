62797e6a7bc9d9ce37a94128f3531e419a513533
Abeermohsien <aboorakhalil993@gmail.com>
2023-12-04 01:32:52 -0800

    first time

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    for i in rev(my_list):
+        print("{:d}".format(i)#!/usr/bin/python3
+                def print_reversed_list_integer(my_list=[]):
+                for i in rev(my_list):
+                print("{:d}".format(i))
