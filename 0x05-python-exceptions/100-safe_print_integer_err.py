63c545c95dd59ba92fbdfaf0ef85c79b689f721c
Abeermohsien <aboorakhalil993@gmail.com>
2023-12-18 03:26:46 -0800

    $

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    is_intt True
+    try:
+        print("{:d}".format(value))
+    except Exceptiom as e:
+        print("Exception:", e, file=sys.stderr)
+        is_intt = False
+    return is_intt
