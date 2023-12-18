e98f1822b46af8decac1ddadfc0a8481f11a8c29
Abeermohsien <aboorakhalil993@gmail.com>
2023-12-18 03:49:46 -0800

    $

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,10 +3,10 @@ import sys
 
 
 def safe_print_integer_err(value):
-    is_intt True
+    i_intt = True
     try:
         print("{:d}".format(value))
     except Exceptiom as e:
         print("Exception:", e, file=sys.stderr)
-        is_intt = False
+        i_intt = False
     return is_intt
