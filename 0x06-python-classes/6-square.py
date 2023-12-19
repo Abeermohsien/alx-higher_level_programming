d8345f460756c56d21e9d4f99e3a3e0c7e681f4c
Abeermohsien <aboorakhalil993@gmail.com>
2023-12-19 08:49:40 -0800

    $

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,7 +4,6 @@
 
 class Square:
     """squre"""
- 
     def __init__(self, size=0, position=(0, 0)):
         """squre"""
         self.size = size
@@ -30,7 +29,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or 
+        if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
@@ -39,7 +38,7 @@ class Square:
 
     def area(self):
         """squre"""
-        return (self.__size * self.__size) 
+        return (self.__size * self.__size)
 
     def my_print(self):
         """sqire"""
