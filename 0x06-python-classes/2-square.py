cdbc8a4f055e2446d836ac69d73ef43a966ed9c6
Abeermohsien <aboorakhalil993@gmail.com>
2023-12-19 03:37:22 -0800

    $

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""Square"""
 
 
-class square:
+class Square:
+    """square"""
     def __init__(self, size=0):
+        """square"""
         if not isinstance(size, int):
             raise TypeError("size must be integer")
         if size < 0:
