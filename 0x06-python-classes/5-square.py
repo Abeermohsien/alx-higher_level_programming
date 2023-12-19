aa495f3179f70fa122da6937f096882637c6c53f
Abeermohsien <aboorakhalil993@gmail.com>
2023-12-19 07:58:05 -0800

    $

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+"""sqare"""
 
 
-class square:
+class Square:
+    """square"""
     def __init__(self, size=0):
+        """square"""
         self.size = size
 
     @property
     def size(self):
+        """square"""
         return self.__size
 
     @size.setter
@@ -18,9 +22,11 @@ class square:
         self.__size = value
 
     def area(self):
-        return self.__size ** 2
+        """square"""
+        return self.__size * self.__size 
 
     def my_print(self):
+        """sqaure"""
         for n in range(self.size):
             for m in range(self.size):
                 print("#", end="\n" if m is self.size - 1 and n != m else "")
