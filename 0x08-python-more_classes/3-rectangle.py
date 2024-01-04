4ef1bae3d95bfcee0aef6164c08d5341b858f694
Abeermohsien <aboorakhalil993@gmail.com>
2024-01-04 09:29:39 -0800

    $

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""
-rectangle
+""""
+calculate area
 """
 
 
 class Rectangle:
     """rectangle"""
     def __init__(self, width=0, height=0):
-        """rectangle"""
+        """rectanglr"""
         self.height = height
         self.width = width
 
@@ -27,12 +27,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """height"""
+        """hwoght"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """width"""
+        """height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -50,9 +50,9 @@ class Rectangle:
         return (self.__width * 2) + (self.__height * 2)
 
     def __str__(self):
-        """string"""
+        """self"""
         string = ""
         if self.__width != 0 and self.__height != 0:
             string += "\n".join("#" * self.__width
-                                for j in range(self.__height))
+                                for i in range(self.__height))
         return string
