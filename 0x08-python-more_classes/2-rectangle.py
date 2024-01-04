1a2e8fdf004e292da5ede13696c7842969432779
Abeermohsien <aboorakhalil993@gmail.com>
2024-01-04 07:45:24 -0800

    $

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -3,6 +3,7 @@
 calculate area
 """
 
+
 class Rectangle:
     """rectangle"""
     def __init__(self, width=0, height=0):
@@ -21,8 +22,7 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0
-")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
         @property
