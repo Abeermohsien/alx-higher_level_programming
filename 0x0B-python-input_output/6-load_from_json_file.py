884dac850efbac991c4b050baae8df4d97d7bf00
Abeermohsien <aboorakhalil993@gmail.com>
2024-01-09 10:16:42 -0800

    $

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,7 +5,7 @@
 import json
 
 
-def save_to_json_file(my_obj, filename):
+def load_from_json_file(filename):
     """save to json"""
     with open(filename, "r", encoding="utf-8") as filee:
         return json.load(filee)
