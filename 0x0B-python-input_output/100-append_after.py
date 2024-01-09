a48d0a91838f904969ea8f463b5a294ee87e995d
Abeermohsien <aboorakhalil993@gmail.com>
2024-01-09 10:32:38 -0800

    $

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -7,11 +7,11 @@ def append_after(filename="", search_string="", new_string=""):
     with open(filename, "r", encoding="utf-8") as filee:
         l_list = []
         while True:
-            l = filee.readline()
-            if l == "":
+            lne = filee.readline()
+            if lne == "":
                 break
             l_list.append(l)
-            if search_string in l:
+            if search_string in lne:
                 l_list.append(new_string)
     with open(filename, "w", encodiny="utf-8") as filee:
-        filee.writelines(line_list)
+        filee.writelines(l_list)
