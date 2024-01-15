d8305a7cc0c660792c83927ee72674cf02e4d16d
Abeermohsien <aboorakhalil993@gmail.com>
2024-01-15 09:34:13 -0800

    $

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -61,4 +61,4 @@ class Rectangle(Base):
         if eq and value < 0:
             raise ValueError("{} must be >= 0".format(name))
         elif not eq and value <= 0:
-            raise ValueError("{} must be > 0".format(name)) 
+            raise ValueError("{} must be > 0".format(name))
