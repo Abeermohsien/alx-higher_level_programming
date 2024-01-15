c977a6a850266972c5051f6d092744380b71244a
Abeermohsien <aboorakhalil993@gmail.com>
2024-01-15 11:33:33 -0800

    $

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -99,3 +99,8 @@ class Rectangle(Base):
             self.__update(*args)
         elif kwargs:
             self.__update(**kwargs)
+
+    def to_dictionary(self):
+        ''' dicti repre'''
+        return {"id": self.id, "width": self.__width, "height": self.__height,
+                "x": self.__x, "y": self.__y}
