e36dbb2e2f60d9cbbd0f0f5385cfb86f48498401
Abeermohsien <aboorakhalil993@gmail.com>
2024-01-15 08:21:28 -0800

    $

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -58,7 +58,7 @@ class Rectangle(Base):
         """validate"""
         if type(value) != int:
             raise TypeError("() must be an integer".format(name))
-        if value < 0 and eq:
+        if eq and value < 0: 
             raise ValueError("() must be >= 0".format(name))
         elif not eq and value <= 0:
             raise ValueError("must be > 0".format(name))
