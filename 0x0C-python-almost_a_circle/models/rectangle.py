56e36c1b04937b89bbfa40a2c12d70974ca4f822
Abeermohsien <aboorakhalil993@gmail.com>
2024-01-15 10:53:19 -0800

    $

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-'''Module for Rectangle class.'''
+'''Module'''
 from models.base import Base
 
 
 class Rectangle(Base):
-    '''A Rectangle class.'''
+    '''rrectangle class.'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        '''inilize'''
+        '''Constructor.'''
         super().__init__(id)
         self.width = width
         self.height = height
@@ -16,7 +16,7 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        '''Width '''
+        '''Width reuctangle.'''
         return self.__width
 
     @width.setter
@@ -26,7 +26,7 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        '''Height'''
+        '''Height reectangle.'''
         return self.__height
 
     @height.setter
@@ -46,7 +46,7 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        '''y '''
+        '''y.'''
         return self.__y
 
     @y.setter
@@ -55,7 +55,7 @@ class Rectangle(Base):
         self.__y = value
 
     def validate_integer(self, name, value, eq=True):
-        '''validating'''
+        '''value.'''
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if eq and value < 0:
@@ -64,43 +64,5 @@ class Rectangle(Base):
             raise ValueError("{} must be > 0".format(name))
 
     def area(self):
-        '''area'''
-        return self.width * self.height
-
-    def display(self):
-        '''str repre'''
-        s = '\n' * self.y + \
-            (' ' * self.x + '#' * self.width + '\n') * self.height
-        print(s, end='')
-
-    def __str__(self):
-        '''string info'''
-        return '[{}] ({}) {}/{} - {}/{}'.\
-            format(type(self).__name__, self.id, self.x, self.y, self.width,
-                   self.height)
-
-    def __update(self, id=None, width=None, height=None, x=None, y=None):
-        '''updatlle attribtes'''
-        if id is not None:
-            self.id = id
-        if width is not None:
-            self.width = width
-        if height is not None:
-            self.height = height
-        if x is not None:
-            self.x = x
-        if y is not None:
-            self.y = y
-
-    def update(self, *args, **kwargs):
-        '''attribtes '''
-        # print(args, kwargs)
-        if args:
-            self.__update(*args)
-        elif kwargs:
-            self.__update(**kwargs)
-
-    def to_dictionary(self):
-        """dir repr"""
-        return {"id": self.id, "width": self.__width, "height": self.__height,
-                "x": self.__x, "y": self.__y}
+        '''area.'''
+        return self.width * self.height 
