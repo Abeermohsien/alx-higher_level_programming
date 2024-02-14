8941d0c00308e8f87ca74628f6ae41f54baaf0a2
Abeermohsien <aboorakhalil993@gmail.com>
2024-02-14 10:20:56 -0800

    $

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- Creta table.
 
-SELECT title, tv_show_genres.genrw_id FROM tv_shows
+SELECT title, tv_show_genres.genre_id FROM tv_shows
 LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
 WHERE tv_show_genres.show_id IS NULL
 ORDER BY title, tv_show_genres.genre_id;
