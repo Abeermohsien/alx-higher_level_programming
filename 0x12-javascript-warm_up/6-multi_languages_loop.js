85a04d425ff049f3d7a91d9234c632857e88005a
Abeermohsien <aboorakhalil993@gmail.com>
2024-03-11 12:42:26 -0700

    $

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-for (const lne of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-   console.log(lne);
+for (const lne of ['C is fun', 'Python is cool',
+'JavaScript is amazing']) {
+  console.log(lne);
 }
