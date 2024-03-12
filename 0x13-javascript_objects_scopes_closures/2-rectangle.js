6cfafcec3b4adb22f941ef4ebb3c2b3b28d6e2c3
Abeermohsien <aboorakhalil993@gmail.com>
2024-03-12 08:00:04 -0700

    $

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if ((h > 0) && (w > 0))
-{
-      this.height = h;
+    if ((h > 0) && (w > 0)) {
+      this.height = h; 
       this.width = w;
     }
   }
