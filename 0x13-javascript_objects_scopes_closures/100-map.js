7df5463c56b0738fb4bfae5e068306f3b46d3245
Abeermohsien <aboorakhalil993@gmail.com>
2024-03-12 12:14:56 -0700

    $

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const m = require('./100-data.js').list;
-console.log(m);
+const list = require('./100-data.js').list;
+console.log(list);
 console.log(list.map((item, index) => item * index));
