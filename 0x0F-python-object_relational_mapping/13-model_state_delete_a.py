039028e1429000adac882119a4cc877c1acee48a
Abeermohsien <aboorakhalil993@gmail.com>
2024-03-17 04:27:21 -0700

    $

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" types state withe name given 
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engcre = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engcre)
+    Ses = sessionmaker(bind=engcre)
+    ses = Session()
+    for n in ses.query(State).filter(State.name.like('%a%')):
+        ses.delete(n)
+    ses.commit()
