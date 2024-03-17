402cb8711e0c0cf866c827f8776d802299eb36c7
Abeermohsien <aboorakhalil993@gmail.com>
2024-03-17 06:29:18 -0700

    $

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""type state object"
-import sys
+"""type state object"""
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
@@ -8,12 +8,12 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           .format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).filter(State.name == (sys.argv[4],))
+    state = session.query(State).filter(State.name == (argv[4],))
     try:
-        print(instance[0].id)
+        print(state[0].id)
     except IndexError:
         print("Not found")
