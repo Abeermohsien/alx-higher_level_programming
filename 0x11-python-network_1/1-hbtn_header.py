e5c391fd3f2993cd623b1229cde352a53604f7c7
Abeermohsien <aboorakhalil993@gmail.com>
2024-03-28 07:26:39 -0700

    first time

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Takes a URL and send a request to it and display a variable value
+"""
+from urllib.request import urlopen 
+from sys import argv
+
+
+if __name__ == "__main__":
+    with urlopen(argv[1]) as response:
+        info = response.info()
+        val = info.get('X-Request-Id')
+        print(val)
